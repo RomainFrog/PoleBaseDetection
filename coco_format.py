d00b8cd4ecb316d16da6ba2f0c0ee75b314892d1
Alexandre Andre <alexandre.andre@etu.utc.fr>
2023-09-13 15:17:43 +0200

    add scpit and data to coco format

--- coco_format.py
+++ coco_format.py
@@ -1,27 +1,68 @@
-from sahi.utils.coco import Coco, CocoCategory, CocoImage, CocoAnnotation
-from sahi.utils.file import save_json
+import csv
+import glob
+import os
+
 from PIL import Image
+from sahi.utils.coco import Coco, CocoAnnotation, CocoCategory, CocoImage
+from sahi.utils.file import save_json
+
+box_w, box_h = 200, 200
+train_per = 0.8
+data_dir = "data_manual_annotations"
+anotation_dir = "annotations_phx"
+img_dir = "images"
+
 
 # Init coco object
-coco = Coco()
+coco_train = Coco()
+coco_val = Coco()
 
 # Add categories
-coco.add_category(CocoCategory(id=0, name='pole'))
+coco_train.add_category(CocoCategory(id=0, name="pole"))
+coco_val.add_category(CocoCategory(id=0, name="pole"))
 
-# Create coco image
-id = str(1652168831032115)
-filename = f"data/images/{id}.jpg"
-height, width = Image.open(filename).size
-coco_image = CocoImage(file_name=filename, height=height, width=width)
 
-coco_image.add_annotation(    
-    CocoAnnotation(    
-        bbox=[10, 20, width, height],    
-        category_id=0,    
-        category_name='human'    
-    )
-)
+anotation_path = os.path.join(data_dir, anotation_dir, "*.csv")
 
-coco.add_image(coco_image)
+csv_files = glob.glob(anotation_path)
 
-save_json(data=coco.json, save_path="coco.json")
+num_train_data = int(len(csv_files) * train_per)
+count = 1
+
+# Loop through each CSV file and append its data to the merged_data DataFrame
+for csv_file in csv_files:
+    with open(csv_file, "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        try:
+            next(reader)
+        except StopIteration:
+            pass
+
+        name_img = os.path.basename(csv_file)[:-3] + "jpg"
+        filename = os.path.join(data_dir, img_dir, name_img)
+
+        height, width = Image.open(filename).size
+        coco_image = CocoImage(
+            file_name=f"{data_dir}/{img_dir}/{name_img}", height=height, width=width
+        )
+
+        for row in reader:
+            x = int(float(row["x"]))
+            y = int(float(row["y"]))
+
+            xtl = max(0, x - box_w // 2)
+            ytl = max(0, y + box_h // 2)
+            coco_image.add_annotation(
+                CocoAnnotation(bbox=[xtl, ytl, box_w, box_h], category_id=0, category_name="pole")
+            )
+
+    if count <= num_train_data:
+        coco_train.add_image(coco_image)
+    else:
+        coco_val.add_image(coco_image)
+    count += 1
+
+
+save_json(data=coco_train.json, save_path="train.json")
+save_json(data=coco_val.json, save_path="val.json")
