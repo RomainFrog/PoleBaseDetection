7483664e10c9a3b572d4b7adf4ce23a6ba55b186
frogerro <romain.froger@etu.utc.fr>
2023-09-22 14:32:11 +0200

    annotation viewer

---
+++ pole_bbox_viz.py
@@ -0,0 +1,51 @@
+import math
+import os
+import cv2
+import sys
+import argparse
+from pathlib import Path
+from typing import Iterable
+from PIL import Image
+import numpy as np
+
+import torch
+
+import matplotlib.pyplot as plt
+import time
+
+from detr.datasets.pole import PoleDetection
+
+# Load dataset
+img_dir = "data_manual_annotations/images"
+ann_dir = "data_manual_annotations/train.json"
+
+dataset = PoleDetection(img_dir, ann_dir, transforms=None, return_masks=False)
+
+# Create a cv2 window to display images from val.json
+cv2.namedWindow("Pole Detection", cv2.WINDOW_NORMAL)
+
+# Display images from val.json
+for i in range(len(dataset)):
+    img, target = dataset[i]
+    img = np.array(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    # display bounding boxes on top of the image
+    for box in target['boxes']:
+        x1, y1, x2, y2 = box
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        print(x1, y1, x2, y2)
+        img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    
+    cv2.imshow("Pole Detection", img)
+    # if key pressed is 'q' then exit the loop
+    if cv2.waitKey(0) == ord('q'):
+        break
+    cv2.waitKey(0)
+
+
+cv2.destroyAllWindows()
+
+
+
+
