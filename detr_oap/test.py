1b02932a69d2d62c7e5663d63baece0071d9025d
Alexandre Andre <alexandre.andre@etu.utc.fr>
2023-10-04 18:25:15 +0200

    update test to have recall and precision metrics

--- detr_oap/test.py
+++ detr_oap/test.py
@@ -2,38 +2,36 @@
 """
 Train and eval functions used in main.py
 """
+import argparse
+import json
 import math
 import os
-import cv2
 import sys
-import argparse
+import time
 from pathlib import Path
 from typing import Iterable
-from PIL import Image
-import numpy as np
-import json
 
-import torch
-
-import util.misc as utils
-
-from models import build_model
-from datasets.pole import make_Pole_transforms
-
+import cv2
 import matplotlib.pyplot as plt
-import time
+import numpy as np
+import torch
+import util.misc as utils
+from datasets.pole import make_Pole_transforms
+from models import build_model
+from PIL import Image
 
 
 def box_cxcywh_to_xyxy(x):
     x_c, y_c, w, h = x.unbind(1)
-    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
-         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h), (x_c + 0.5 * w), (y_c + 0.5 * h)]
     return torch.stack(b, dim=1)
+
 
 def rescale_bboxes(out_bbox, size):
     img_w, img_h = size
     b = out_bbox * torch.tensor([img_w, img_h], dtype=torch.float32)
     return b
+
 
 def get_images(in_path):
     img_files = []
@@ -41,86 +39,129 @@ def get_images(in_path):
         for file in filenames:
             filename, ext = os.path.splitext(file)
             ext = str.lower(ext)
-            if ext == '.jpg' or ext == '.jpeg' or ext == '.gif' or ext == '.png' or ext == '.pgm':
+            if ext == ".jpg" or ext == ".jpeg" or ext == ".gif" or ext == ".png" or ext == ".pgm":
                 img_files.append(os.path.join(dirpath, file))
 
     return img_files
 
 
 def get_args_parser():
-    parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
-    parser.add_argument('--lr', default=1e-4, type=float)
-    parser.add_argument('--lr_backbone', default=1e-5, type=float)
-    parser.add_argument('--batch_size', default=6, type=int)
-    parser.add_argument('--weight_decay', default=1e-4, type=float)
-    parser.add_argument('--epochs', default=300, type=int)
-    parser.add_argument('--lr_drop', default=200, type=int)
-    parser.add_argument('--clip_max_norm', default=0.1, type=float,
-                        help='gradient clipping max norm')
+    parser = argparse.ArgumentParser("Set transformer detector", add_help=False)
+    parser.add_argument("--lr", default=1e-4, type=float)
+    parser.add_argument("--lr_backbone", default=1e-5, type=float)
+    parser.add_argument("--batch_size", default=6, type=int)
+    parser.add_argument("--weight_decay", default=1e-4, type=float)
+    parser.add_argument("--epochs", default=300, type=int)
+    parser.add_argument("--lr_drop", default=200, type=int)
+    parser.add_argument(
+        "--clip_max_norm", default=0.1, type=float, help="gradient clipping max norm"
+    )
 
     # Model parameters
-    parser.add_argument('--frozen_weights', type=str, default=None,
-                        help="Path to the pretrained model. If set, only the mask head will be trained")
+    parser.add_argument(
+        "--frozen_weights",
+        type=str,
+        default=None,
+        help="Path to the pretrained model. If set, only the mask head will be trained",
+    )
     # * Backbone
-    parser.add_argument('--backbone', default='resnet50', type=str,
-                        help="Name of the convolutional backbone to use")
-    parser.add_argument('--dilation', action='store_true',
-                        help="If true, we replace stride with dilation in the last convolutional block (DC5)")
-    parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned'),
-                        help="Type of positional embedding to use on top of the image features")
+    parser.add_argument(
+        "--backbone",
+        default="resnet50",
+        type=str,
+        help="Name of the convolutional backbone to use",
+    )
+    parser.add_argument(
+        "--dilation",
+        action="store_true",
+        help="If true, we replace stride with dilation in the last convolutional block (DC5)",
+    )
+    parser.add_argument(
+        "--position_embedding",
+        default="sine",
+        type=str,
+        choices=("sine", "learned"),
+        help="Type of positional embedding to use on top of the image features",
+    )
 
     # * Transformer
-    parser.add_argument('--enc_layers', default=6, type=int,
-                        help="Number of encoding layers in the transformer")
-    parser.add_argument('--dec_layers', default=6, type=int,
-                        help="Number of decoding layers in the transformer")
-    parser.add_argument('--dim_feedforward', default=2048, type=int,
-                        help="Intermediate size of the feedforward layers in the transformer blocks")
-    parser.add_argument('--hidden_dim', default=256, type=int,
-                        help="Size of the embeddings (dimension of the transformer)")
-    parser.add_argument('--dropout', default=0.1, type=float,
-                        help="Dropout applied in the transformer")
-    parser.add_argument('--nheads', default=8, type=int,
-                        help="Number of attention heads inside the transformer's attentions")
-    parser.add_argument('--num_queries', default=20, type=int,
-                        help="Number of query slots")
-    parser.add_argument('--pre_norm', action='store_true')
+    parser.add_argument(
+        "--enc_layers", default=6, type=int, help="Number of encoding layers in the transformer"
+    )
+    parser.add_argument(
+        "--dec_layers", default=6, type=int, help="Number of decoding layers in the transformer"
+    )
+    parser.add_argument(
+        "--dim_feedforward",
+        default=2048,
+        type=int,
+        help="Intermediate size of the feedforward layers in the transformer blocks",
+    )
+    parser.add_argument(
+        "--hidden_dim",
+        default=256,
+        type=int,
+        help="Size of the embeddings (dimension of the transformer)",
+    )
+    parser.add_argument(
+        "--dropout", default=0.1, type=float, help="Dropout applied in the transformer"
+    )
+    parser.add_argument(
+        "--nheads",
+        default=8,
+        type=int,
+        help="Number of attention heads inside the transformer's attentions",
+    )
+    parser.add_argument("--num_queries", default=20, type=int, help="Number of query slots")
+    parser.add_argument("--pre_norm", action="store_true")
 
     # * Segmentation
-    parser.add_argument('--masks', action='store_true',
-                        help="Train segmentation head if the flag is provided")
+    parser.add_argument(
+        "--masks", action="store_true", help="Train segmentation head if the flag is provided"
+    )
 
     # # Loss
-    parser.add_argument('--no_aux_loss', dest='aux_loss', action='store_false',
-                        help="Disables auxiliary decoding losses (loss at each layer)")
+    parser.add_argument(
+        "--no_aux_loss",
+        dest="aux_loss",
+        action="store_false",
+        help="Disables auxiliary decoding losses (loss at each layer)",
+    )
     # * Matcher
-    parser.add_argument('--set_cost_class', default=1, type=float,
-                        help="Class coefficient in the matching cost")
-    parser.add_argument('--set_cost_dist', default=5, type=float,
-                        help="L1 box coefficient in the matching cost")
-    parser.add_argument('--set_cost_giou', default=2, type=float,
-                        help="giou box coefficient in the matching cost")
+    parser.add_argument(
+        "--set_cost_class", default=1, type=float, help="Class coefficient in the matching cost"
+    )
+    parser.add_argument(
+        "--set_cost_dist", default=5, type=float, help="L1 box coefficient in the matching cost"
+    )
+    parser.add_argument(
+        "--set_cost_giou", default=2, type=float, help="giou box coefficient in the matching cost"
+    )
     # * Loss coefficients
-    parser.add_argument('--mask_loss_coef', default=1, type=float)
-    parser.add_argument('--dice_loss_coef', default=1, type=float)
-    parser.add_argument('--bbox_loss_coef', default=5, type=float)
-    parser.add_argument('--giou_loss_coef', default=2, type=float)
-    parser.add_argument('--eos_coef', default=0.1, type=float,
-                        help="Relative classification weight of the no-object class")
+    parser.add_argument("--mask_loss_coef", default=1, type=float)
+    parser.add_argument("--dice_loss_coef", default=1, type=float)
+    parser.add_argument("--bbox_loss_coef", default=5, type=float)
+    parser.add_argument("--giou_loss_coef", default=2, type=float)
+    parser.add_argument(
+        "--eos_coef",
+        default=0.1,
+        type=float,
+        help="Relative classification weight of the no-object class",
+    )
 
     # dataset parameters
-    parser.add_argument('--dataset_file', default='pole')
-    parser.add_argument('--data_path', type=str)
-    parser.add_argument('--data_panoptic_path', type=str)
-    parser.add_argument('--remove_difficult', action='store_true')
+    parser.add_argument("--dataset_file", default="pole")
+    parser.add_argument("--data_path", type=str)
+    parser.add_argument("--data_panoptic_path", type=str)
+    parser.add_argument("--remove_difficult", action="store_true")
 
-    parser.add_argument('--output_dir', default='',
-                        help='path where to save the results, empty for no saving')
-    parser.add_argument('--device', default='cuda',
-                        help='device to use for training / testing')
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument(
+        "--output_dir", default="", help="path where to save the results, empty for no saving"
+    )
+    parser.add_argument("--device", default="cuda", help="device to use for training / testing")
+    parser.add_argument("--resume", default="", help="resume from checkpoint")
 
-    parser.add_argument('--thresh', default=0.5, type=float)
+    parser.add_argument("--thresh", default=0.5, type=float)
 
     return parser
 
@@ -129,64 +170,76 @@ def get_args_parser():
 def infer(images_path, model, postprocessors, device, output_path):
     # load grount truth json from data_manual_annotations/val.json
     gt = "../data_manual_annotations/annotations_tx_reviewed_final"
-    
+
     model.eval()
     duration = 0
+    total_tp = 0
+    total_fp = 0
+    total_fn = 0
+
+    total = 0
+
     for img_sample in images_path:
         filename = os.path.basename(img_sample)
-        print("processing...{}".format(filename))
         # get the file name without extension
         file_basename = os.path.splitext(filename)[0]
         # get ground truth from csv file
         gt_file = os.path.join(gt, file_basename + ".csv")
         if not os.path.exists(gt_file):
             continue
-        # read csv file data (skip first line)
-        gt_data = np.genfromtxt(gt_file, delimiter=',', skip_header=1).astype(np.int32)
+
+        print("processing...{}".format(filename))
+        # if the file doesn't have any ground truth
+        if os.path.getsize(gt_file) <= 14:
+            gt_data = []
+        else:
+            # read csv file data (skip first line)
+            gt_data = np.genfromtxt(gt_file, delimiter=",", skip_header=1, usecols=[1, 2]).astype(
+                np.int32
+            )
+
         orig_image = Image.open(img_sample)
         w, h = orig_image.size
         transform = make_Pole_transforms("val")
         dummy_target = {
             "size": torch.as_tensor([int(h), int(w)]),
-            "orig_size": torch.as_tensor([int(h), int(w)])
+            "orig_size": torch.as_tensor([int(h), int(w)]),
         }
         image, targets = transform(orig_image, dummy_target)
         image = image.unsqueeze(0)
         image = image.to(device)
 
-
         conv_features, enc_attn_weights, dec_attn_weights = [], [], []
         hooks = [
             model.backbone[-2].register_forward_hook(
-                        lambda self, input, output: conv_features.append(output)
-
+                lambda self, input, output: conv_features.append(output)
             ),
             model.transformer.encoder.layers[-1].self_attn.register_forward_hook(
-                        lambda self, input, output: enc_attn_weights.append(output[1])
-
+                lambda self, input, output: enc_attn_weights.append(output[1])
             ),
             model.transformer.decoder.layers[-1].multihead_attn.register_forward_hook(
-                        lambda self, input, output: dec_attn_weights.append(output[1])
-
+                lambda self, input, output: dec_attn_weights.append(output[1])
             ),
-
         ]
 
+        print("Start inference")
         start_t = time.perf_counter()
         outputs = model(image)
         end_t = time.perf_counter()
+        infer_time = end_t - start_t
+        duration += infer_time
+        print("End inference ({:.3f}s)".format(infer_time))
 
         outputs["pred_logits"] = outputs["pred_logits"].cpu()
         outputs["pred_boxes"] = outputs["pred_boxes"].cpu()
 
-        probas = outputs['pred_logits'].softmax(-1)[0, :, :-1]
+        probas = outputs["pred_logits"].softmax(-1)[0, :, :-1]
         # keep = probas.max(-1).values > 0.85
         keep = probas.max(-1).values > args.thresh
 
         # Scale bbox to the same size as the original
-        bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], orig_image.size)
+        bboxes_scaled = rescale_bboxes(outputs["pred_boxes"][0, keep], orig_image.size)
         probas = probas[keep].cpu().data.numpy()
-
 
         for hook in hooks:
             hook.remove()
@@ -196,36 +249,91 @@ def infer(images_path, model, postprocessors, device, output_path):
         dec_attn_weights = dec_attn_weights[0].cpu()
 
         # get the feature map shape
-        h, w = conv_features['0'].tensors.shape[-2:]
+        h, w = conv_features["0"].tensors.shape[-2:]
 
-        if len(bboxes_scaled) == 0:
-            continue
+        print("Start matching")
+        l_tp, l_fp, l_fn = get_tp_fp_fn(bboxes_scaled, probas, gt_data, 20)
+        print("End matching")
 
-        img = np.array(orig_image)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        for idx, box in enumerate(bboxes_scaled):
-            bbox = box.cpu().data.numpy()
-            bbox = bbox.astype(np.int32)
-            cv2.circle(img, (bbox[0], bbox[1]), 2, (0, 0, 255), 2)
+        total_tp += len(l_tp)
+        total_fp += len(l_fp)
+        total_fn += len(l_fn)
 
-        for p in gt_data:
-            p = p[1:3]
-            cv2.circle(img, (p[0], p[1]), 2, (0, 255, 0), 2)
+        # if len(l_tp) + len(l_fp) + len(l_fn) == 0:
+        #     continue
 
-        # img_save_path = os.path.join(output_path, filename)
-        # cv2.imwrite(img_save_path, img)
-        cv2.imshow("img", img)
-        cv2.waitKey()
-        infer_time = end_t - start_t
-        duration += infer_time
-        print("Processing...{} ({:.3f}s)".format(filename, infer_time))
+        # img = np.array(orig_image)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # for tp in l_tp:
+        #     cv2.circle(img, (int(tp[0]), int(tp[1])), 2, (0, 0, 255), 2)  # red
+
+        # for fp in l_fp:
+        #     cv2.circle(img, (int(fp[0]), int(fp[1])), 2, (255, 0, 0), 2)  # blue
+
+        # for fn in l_fn:
+        #     cv2.circle(img, (int(fn[0]), int(fn[1])), 2, (0, 255, 0), 2)  # green
+
+        # # img_save_path = os.path.join(output_path, filename)
+        # # cv2.imwrite(img_save_path, img)
+        # cv2.imshow("img", img)
+        # cv2.waitKey()
+
+        print(f"End processing {filename}")
+
+    # compute precision and recall
+    recall, precision = get_recall_precision(total_tp, total_fp, total_fn)
 
     avg_duration = duration / len(images_path)
     print("Avg. Time: {:.3f}s".format(avg_duration))
+    print("Recall: {:.3f}, Precision: {:.3f}".format(recall, precision))
+
+
+# function that return number of true positives, false positives and false negatives
+def get_tp_fp_fn(pred, probas, gt, thresh):
+    l_tp, l_fp, l_fn = [], [], []
+
+    pred = pred.numpy()
+    print(pred)
+    print(probas)
+    idx = np.argsort(probas.flatten())[::-1]
+    pred = pred[idx]
+    print(pred)
+    print(gt)
+
+    pred_copy = pred.copy()
+    gt_copy = gt.copy()
+
+    for p in pred_copy:
+        for g in gt_copy:
+            dist = np.linalg.norm(p - g)
+            if dist < thresh:
+                gt_copy = np.delete(gt_copy, np.where(gt_copy == g)[0], axis=0)
+                pred_copy = np.delete(pred_copy, np.where(pred_copy == p)[0], axis=0)
+                l_tp.append(p)
+                break
+        else:
+            l_fp.append(p)
+
+    for g in gt_copy:
+        l_fn.append(g)
+
+    print("tp: {}, fp: {}, fn: {}".format(len(l_tp), len(l_fp), len(l_fn)))
+
+    return l_tp, l_fp, l_fn
+
+
+# get recall and precision
+def get_recall_precision(tp, fp, fn):
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+
+    return recall, precision
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('DETR training and evaluation script', parents=[get_args_parser()])
+    parser = argparse.ArgumentParser(
+        "DETR training and evaluation script", parents=[get_args_parser()]
+    )
     args = parser.parse_args()
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
@@ -234,8 +342,8 @@ if __name__ == "__main__":
 
     model, _, postprocessors = build_model(args)
     if args.resume:
-        checkpoint = torch.load(args.resume, map_location='cpu')
-        model.load_state_dict(checkpoint['model'])
+        checkpoint = torch.load(args.resume, map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
     model.to(device)
     image_paths = get_images(args.data_path)
 
